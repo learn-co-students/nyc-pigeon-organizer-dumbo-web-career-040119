927cf4538a2d08c5ef997bef35277420c843ae1e
ftse105 <ftse105@gmail.com>
2019-03-27 17:31:37 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,44 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+def nyc_pigeon_organizer (data)
+  new_data = {}
+
+  data.each do |first_level, all_other|
+    all_other.each do |category, array|
+      array.each do |name|
+        new_data[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+
+  x = new_data.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          new_data[item][:color] << bird_color.to_s
+        end
+      end
+    end
+  end
+
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          new_data[item][:gender] << gender.to_s
+        end
+      end
+    end
+  end
+  
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          new_data[item][:lives] << location
+        end
+      end
+    end
+  end
+
+  return new_data
 end
