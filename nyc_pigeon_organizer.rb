3b5b6d224766cb15613223e698c80c4244244e4e
cogilvy <chrisogilvy@cox.net>
2019-03-28 18:21:35 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized = {}
+  data.each do |characteristic, values|
+    values.each do |value, pigeons_array|
+      pigeons_array.each do |bird_name|
+        organized[bird_name] ||= {}
+        organized[bird_name][characteristic] ||= []
+        organized[bird_name][characteristic] << value.to_s
+      end
+    end
+  end
+  organized
 end
